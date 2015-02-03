264465fd96fd5488e2cc63f70113d9b653543e35
DanielVarzim <danielvarzim@hotmail.com>
2015-02-03 20:24:19 +0000

    Efectua o blast de todos os genes da nossa região

--- Scripts/Blast.py
+++ Scripts/Blast.py
@@ -3,7 +3,7 @@
 '''
 Created on 29/01/2015
 
-@author: Daniel Varzim
+@author: Utilizando parte do script do grupo 5 e exemplos do tutorial de biopython
 '''
 
 from Bio.Blast import NCBIWWW
@@ -20,34 +20,51 @@ def blast(record,filename):
     save_file.write(result_handle.read())
     save_file.close()
     result_handle.close()
-    #Não consegui mover o ficheiro criado para a pasta desejada
-    """
-    #moving the file to another directory
-    path=os.getcwd()
-    src = path+"/"+filename #source folder
-    dst = "C:/Users/Daniel Varzim/Documents/GitHub/Trab_Lab_Algoritmos/Resultados/Blast"#destination folder
-    shutil.move(src, dst)
-    """
+    
 
+
+def get_locustag(record):
+    gis = []
+    
+    #Codigo do grupo 5
+    # selects our relevant locus tags, between NGO1213 and NGO1455, and adds them to 'gis'
+    for feature in record.features:
+        if feature.type == 'CDS':
+            locus_tag = feature.qualifiers['locus_tag'][0]
+            if locus_tag >= 'NGO1213' and locus_tag <= 'NGO1455':
+                gis.append(feature.qualifiers['db_xref'][0][3:])
+                
+    return gis
+
+def blastp(genes):
+    for gix in genes:
+        result_handle = NCBIWWW.qblast("blastp", "swissprot", gix)
+        save_file = open(gix+"_blast.xml", "w")
+        save_file.write(result_handle.read())
+        save_file.close()
+        result_handle.close()
+        
+        #moving the file to another directory
+        path=os.getcwd()
+        src = path+"/"+gix+"_blast.xml" #source folder
+        dst = "C:/Users/Daniel Varzim/Documents/GitHub/Trab_Lab_Algoritmos/Resultados/Blast"#destination folder
+        shutil.move(src, dst)
+        
 
 def teste1():
-    #Efectua o blast de um ficheiro
+    #Efectua o blast de um único ficheiro gb gerado, por exemplo:
     #C:\Users\Daniel Varzim\Documents\GitHub\Trab_Lab_Algoritmos\Resultados\gb records\NGO1213
-    
     #filename= input("Introduza o nome do ficheiro a efectuar o blast:")
     #record = SeqIO.read(filename+".gb", format="genbank")
     #blast(record, filename)
     
 
-    #tentativa de efectuar o blast de todos os ficheiros genbank gerados
-    #não pode ser um intervalo entre 1213 e 1456 uma vez que alguns valores não existem
-    #arranjar outra forma
+    #tentativa de efectuar o blast de todos os genes
     
-    for i in range(1213,1456):
-        a=str(i)
-        filename= ("C:\\Users\\Daniel Varzim\\Documents\\GitHub\\Trab_Lab_Algoritmos\\Resultados\\gb records\\NGO"+a)
-        record = SeqIO.read(filename+".gb", format="genbank")
-        blast(record, filename)
+    filename= ("C:\\Users\\Daniel Varzim\\Documents\\GitHub\\Trab_Lab_Algoritmos\\Resultados\\Regiao")
+    record = SeqIO.read(filename+".gb", format="genbank")
+    genes=get_locustag(record)
+    blastp(genes)
     
 
     
